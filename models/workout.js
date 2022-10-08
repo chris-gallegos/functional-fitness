7e31e276e65bc37cddbb72c57cd36cb7c1a657fe
chris-gallegos <chrisgallegos101@gmail.com>
2022-10-07 17:48:48 -0700

    added some comment UI on show page

--- models/workout.js
+++ models/workout.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose');
 // optional shortcut variable
 const Schema = mongoose.Schema;
 
+
+const commentsSchema = new Schema({
+    content: String,
+    user: {type: Schema.Types.ObjectId, ref: 'User', required: true },
+    userName: String,
+    userAvatar: String
+}, {
+    timestamps: true
+})
+
+
 const workoutSchema = new Schema ({
     title: String,
     about: String,
@@ -9,7 +20,8 @@ const workoutSchema = new Schema ({
     difficulyLevel: { 
         type: String,
         enum: ['easy', 'medium', 'hard']
-    }
+    },
+    comments: [commentsSchema]
 });
 
 
